fc5654842a325da761597c5e69c56ce8fddd6434
git <geresmithdavid@gmail.com>
2020-05-05 23:31:25 +0200

    up

--- src/Content.ts
+++ src/Content.ts
@@ -25,7 +25,7 @@ export default class Content {
 
         // Kezd a kódolást innen -->
         res.write("próba\n");
-        res.write("nem alma\n");
+        res.write("de mégis alma\n");
         // <---- Fejezd be a kódolást
 
         res.write("</pre></form></body></html>");
